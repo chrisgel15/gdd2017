6fcc9e27a7e6b07449bf331a90b94efbe6e05c8e
Christian Gelman <christiangelman@gmail.com>
2017-05-13 14:02:54 -0300

    Se sube la aplicacion Desktop

---
+++ app/UberFrba/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UberFrba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // ExecuteStoreProcedure();
+        }
+              public void ExecuteStoreProcedure()
+        {
+            //using (SqlConnection conn = new SqlConnection("Data Source=localhost\\SQLSERVER2012;Initial Catalog=GD1C2017;User ID=gd;Password=gestiondedatos2017!!"))
+            //{
+            //    conn.Open();
+            //    SqlCommand cmd = new SqlCommand("insertTestTable", conn);
+            //    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            //    cmd.Parameters.AddWithValue("@nombre", "nombreBlaBla");
+            //    cmd.ExecuteNonQuery();
+            //}
+        }
+    }
+}
