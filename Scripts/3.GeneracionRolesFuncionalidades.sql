7248a3a43f55f09b5e8f1e1a12e4fc9b6115c4e9
Christian Gelman <christiangelman@gmail.com>
2017-05-14 18:28:18 -0300

    Generacion Roles y Funcionalidades

---
+++ Scripts/3.GeneracionRolesFuncionalidades.sql
@@ -0,0 +1,20 @@
+-- Generacion Roles y Funcionalidades
+
+insert into OLA_K_ASE.ROLES (NOMBRE, HABILITADO) values ('Administrador', 1)
+insert into OLA_K_ASE.ROLES (NOMBRE, HABILITADO) values ('Cliente', 1)
+insert into OLA_K_ASE.ROLES (NOMBRE, HABILITADO) values ('Chofer', 1)
+
+
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Rol')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Login y Seguridad')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Registro de Usuario')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Cliente')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Automóvil')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Chofer')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Registro de Viajes')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Rendición de cuenta del chofer')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Facturación a Cliente')
+insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Listado Estadístico')
+
+
+
