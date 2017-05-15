ba1ee7b6d994749d0d00978d4013fc76187de5de
Lucía González Querzola <luciagonzalezq@gmail.com>
2017-05-14 22:51:14 -0300

    Generacion de usuarios

---
+++ Scripts/9.GeneracionUsuarios.sql
@@ -0,0 +1,66 @@
+----CARGA USUARIOS CON CHOFERES ---
+
+begin
+
+insert into OLA_K_ASE.USUARIOS(NOMBRE, PASSWORD, CANT_FALLAS)
+select distinct DNI, HashBytes('SHA2_256',convert(varchar(255), DNI)), 0 from OLA_K_ASE.choferes
+
+declare @cantidad_usuarios int
+declare @contador int
+declare @id_usuario int
+declare @nombre_usuario varchar(50)
+
+set @cantidad_usuarios = (select count(*) from OLA_K_ASE.USUARIOS)
+set @contador = 1
+
+while @contador <= @cantidad_usuarios
+begin
+set @nombre_usuario = (select NOMBRE from OLA_K_ASE.USUARIOS where ID_USUARIO = @contador)
+
+update OLA_K_ASE.Maestra_Stg1
+set USUARIO_ID = @contador
+where Chofer_DNI = @nombre_usuario
+
+update OLA_K_ASE.CHOFERES
+set USUARIO_ID = @contador
+where DNI = @nombre_usuario
+
+set @contador = @contador + 1
+
+end
+
+end
+
+--- CARGA USUARIOS CON CLIENTES ---
+begin
+
+insert into OLA_K_ASE.USUARIOS(NOMBRE, PASSWORD, CANT_FALLAS)
+select distinct DNI,HashBytes('SHA2_256',convert(varchar(255), DNI)), 0 from OLA_K_ASE.clientes
+
+declare @cantidad_usuariosCL int
+declare @contadorCL int
+declare @id_usuarioCL int
+declare @nombre_usuarioCL varchar(50)
+
+set @cantidad_usuariosCL = (select count(*) from OLA_K_ASE.USUARIOS)
+set @contadorCL = (select max(id_usuario) from OLA_K_ASE.USUARIOS)+1
+
+while @contadorCL <= @cantidad_usuariosCL
+begin
+set @nombre_usuarioCL = (select NOMBRE from OLA_K_ASE.USUARIOS where ID_USUARIO = @contadorCL)
+
+update OLA_K_ASE.Maestra_Stg1
+set USUARIO_ID = @contadorCL
+where Cliente_DNI = @nombre_usuarioCL
+
+update OLA_K_ASE.CLIENTES
+set USUARIO_ID = @contadorCL
+where DNI = @nombre_usuarioCL
+
+set @contadorCL = @contadorCL + 1
+
+end
+
+
+end;
+
