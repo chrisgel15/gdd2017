2e549eb08d496d0c0bea37db20dc6b5f09d51bfd
Christian Gelman <christiangelman@gmail.com>
2017-05-16 16:41:56 -0300

    merge

---
+++ Scripts/10.Roles_x_Usuario.sql
@@ -0,0 +1,9 @@
+-- Migracion ROLES_X_USUARIOS
+
+begin
+
+insert into OLA_K_ASE.ROLES_USUARIOS(USUARIO_ID, ROLES_ID)
+select USUARIO_ID, 2 from OLA_K_ASE.CLIENTES
+union
+select usuario_id, 3 from OLA_K_ASE.CHOFERES
+end;
