740ec0a732b2ffd108b909b6931d807e3100616c
Christian Gelman <christiangelman@gmail.com>
2017-06-10 10:01:02 -0300

    quito la funcionalidad del login de la tabla

--- Scripts/script_creacion_inicial.sql
+++ Scripts/script_creacion_inicial.sql
@@ -740,7 +740,6 @@ insert into OLA_K_ASE.ROLES (NOMBRE, HABILITADO) values ('Chofer', 1)
 
 
 insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Rol')
-insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Login y Seguridad')
 insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('Registro de Usuario')
 insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Cliente')
 insert into OLA_K_ASE.FUNCIONALIDADES (NOMBRE) values ('ABM de Automóvil')
